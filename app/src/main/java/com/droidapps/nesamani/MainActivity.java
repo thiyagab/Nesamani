f5e33e3b4fc61e70a0a29c685558c73d2321c627
thiyagab <1@thiyagab.com>
2019-05-31 01:20:22 +0530

    version 1.0 uploaded to playstore

--- app/src/main/java/com/droidapps/nesamani/MainActivity.java
+++ app/src/main/java/com/droidapps/nesamani/MainActivity.java
@@ -1,6 +1,8 @@
 package com.droidapps.nesamani;
 
 import android.animation.ValueAnimator;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.NonNull;
@@ -9,9 +11,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -47,66 +52,76 @@ public class MainActivity extends AppCompatActivity {
         int counterValue=Integer.parseInt(counter.getText().toString());
         Map counterMap = new HashMap();
         counterMap.put("count",counterValue);
+        saveLocal(counterValue);
         FirebaseFirestore.getInstance().collection("praycount").document("byUC5qAvb2YFJq5uyYO2").set(counterMap);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(counter!=null){
+            setCounterValue(getLocal());
+        }
+    }
+
+    public void saveLocal(int counterValue){
+        getPreferences(MODE_PRIVATE).edit().putInt("count",counterValue).commit();
+    }
+
+    public int getLocal(){
+       return getPreferences(MODE_PRIVATE).getInt("count",1345);
+    }
+
+    public void incrementMyLocal(){
+        int mycount=getMyLocal();
+        mycount++;
+        getPreferences(MODE_PRIVATE).edit().putInt("mycount",mycount).commit();
+    }
+
+    public int getMyLocal(){
+        return getPreferences(MODE_PRIVATE).getInt("mycount",0);
+    }
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-                findViewById(R.id.share).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.image).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                try {
-                    sendMessage();
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
+            public void onClick(View v) {
+                increment();
             }
         });
-                counter = findViewById(R.id.counter);
-                pray = findViewById(R.id.pray);
-                pray.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        increment();
-                    }
-                });
+        findViewById(R.id.share).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-   initDB();
+                    sendMessage();
 
+            }
+        });
+        counter = findViewById(R.id.counter);
+        pray = findViewById(R.id.pray);
+        pray.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                increment();
+            }
+        });
+
+        fetchFromDB();
+        setCounterValue(getLocal());
 
     }
 
-    private void initDB() {
+    private void fetchFromDB() {
+        Toast.makeText(this,"Connecting to world...",Toast.LENGTH_SHORT).show();
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        // Create a new user with a first, middle, and last name
-        Map<String, Object> user = new HashMap<>();
-        user.put("first", "Alan");
-        user.put("middle", "Mathison");
-        user.put("last", "Turing");
-
-
-// Add a new document with a generated ID
-        db.collection("users")
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding document", e);
-                    }
-                });
-
-//        db.collection("praycount")
-//                .where("count", ">", 0)
-//                .orderBy("count", "asc")
-        // Create a new user with a first and last name
         db.collection("praycount").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
@@ -118,8 +133,7 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     Log.w(TAG, "Error getting documents.", task.getException());
                 }
-//                Log.d(TAG, "Total Count: " + snapshots.getDocuments().get(0).get("count"));
-//                ((TextView)findViewById(R.id.counter)).setText(""+snapshots.getDocuments().get(0).get("count"));
+
             }
         });
 
@@ -128,15 +142,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void setCounterValue( int counterValue){
-//        ValueAnimator animator = new ValueAnimator();
-//        animator.setObjectValues(0, counterValue);// here you set the range, from 0 to "count" value
-//        animator.addUpdateListener(new AnimatorUpdateListener() {
-//            public void onAnimationUpdate(ValueAnimator animation) {
-//                highscoretext.setText(String.valueOf(animation.getAnimatedValue()));
-//            }
-//        });
-//        animator.setDuration(1000); // here you set the duration of the anim
-//        animator.start();
         counter.setText(""+counterValue);
     }
 
@@ -144,20 +149,40 @@ public class MainActivity extends AppCompatActivity {
        String text= counter.getText().toString();
        int counterValue=Integer.parseInt(text);
        counterValue++;
-       counter.setText(""+counterValue);
+       incrementMyLocal();
+       setCounterValue(counterValue);
     }
 
-    public void sendMessage() throws FileNotFoundException {
-        String whatsAppMessage = "Hello my friend";
+
+
+    public void sendMessage()  {
+        saveToCloud();
+
         //You can read the image from external drove too
-        Uri uri = Uri.parse("android.resource://com.droidapps.nesamani/drawable/web_hi_res_512.png");
-        Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_SEND);
-        intent.putExtra(Intent.EXTRA_TEXT, whatsAppMessage);
-        intent.setType("text/plain");
-        intent.putExtra(Intent.EXTRA_STREAM,uri);
-        intent.setType("image/jpeg");
-        startActivity(intent);
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "Pray with me for Nesamani."+" "+getLocal()+" Prayers so far."+ "\nhttps://play.google.com/store/apps/details?id="+getPackageName());
+        sendIntent.setType("text/plain");
+        startActivity(sendIntent);
+
     }
 
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder adb = new AlertDialog.Builder(this);
+        adb.setMessage("You have done "+getMyLocal()+"  prayers so far.  \nSave Nesamani with more prayers, Invite your friends to pray");
+        adb.setTitle("Pray more");
+        adb.setPositiveButton("Invite", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+               sendMessage();
+            }
+        });
+        adb.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+                MainActivity.super.onBackPressed();
+            }
+        });
+        adb.show();
+    }
 }
