6130fae31cd006febe33f4e35aecbba8b915959a
thiyagab <1@thiyagab.com>
2019-05-31 10:23:54 +0530

    added code for hits vs prayers

--- app/src/main/java/com/droidapps/nesamani/MainActivity.java
+++ app/src/main/java/com/droidapps/nesamani/MainActivity.java
@@ -1,19 +1,15 @@
 package com.droidapps.nesamani;
 
-import android.animation.ValueAnimator;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,19 +18,12 @@ import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.FirebaseFirestoreSettings;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 
-import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -42,7 +31,9 @@ public class MainActivity extends AppCompatActivity {
 
     static final String TAG="nesamani";
     TextView counter;
+    TextView hits;
     ImageButton pray;
+    ImageButton hammer;
 
     @Override
     protected void onPause() {
@@ -53,9 +44,14 @@ public class MainActivity extends AppCompatActivity {
 
     private void saveToCloud() {
         int counterValue=Integer.parseInt(counter.getText().toString());
+        int hitsValue=Integer.parseInt(hits.getText().toString());
         Map counterMap = new HashMap();
         counterMap.put("count",counterValue);
+        counterMap.put("hits",hitsValue);
         saveLocal(counterValue);
+        saveLocalHits(hitsValue);
+
+
         FirebaseFirestore.getInstance().collection("praycount").document("byUC5qAvb2YFJq5uyYO2").set(counterMap);
     }
 
@@ -71,18 +67,36 @@ public class MainActivity extends AppCompatActivity {
         getPreferences(MODE_PRIVATE).edit().putInt("count",counterValue).commit();
     }
 
-    public int getLocal(){
-       return getPreferences(MODE_PRIVATE).getInt("count",1345);
+    public void saveLocalHits(int counterValue){
+        getPreferences(MODE_PRIVATE).edit().putInt("hits",counterValue).commit();
     }
 
-    public void incrementMyLocal(){
+    public int getLocal(){
+       return getPreferences(MODE_PRIVATE).getInt("count",10234);
+    }
+
+    public int getLocalHits(){
+        return getPreferences(MODE_PRIVATE).getInt("hits",10234);
+    }
+
+    public void incrementMyLocalPrayers(){
         int mycount=getMyLocal();
         mycount++;
         getPreferences(MODE_PRIVATE).edit().putInt("mycount",mycount).commit();
     }
 
+    public void incrementMyLocalHits(){
+        int mycount=getMyHitsLocal();
+        mycount++;
+        getPreferences(MODE_PRIVATE).edit().putInt("myhitcount",mycount).commit();
+    }
+
     public int getMyLocal(){
         return getPreferences(MODE_PRIVATE).getInt("mycount",0);
+    }
+
+    public int getMyHitsLocal(){
+        return getPreferences(MODE_PRIVATE).getInt("myhitcount",0);
     }
 
 
@@ -93,7 +107,7 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_new);
 
 
         MobileAds.initialize(this, "ca-app-pub-4898754855127691~3616544402");
@@ -101,7 +115,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.image).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                increment();
+                incrementPrayers();
             }
         });
         findViewById(R.id.share).setOnClickListener(new View.OnClickListener() {
@@ -113,16 +127,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         counter = findViewById(R.id.counter);
+        hits = findViewById(R.id.hammercounter);
         pray = findViewById(R.id.pray);
+
+        hammer = findViewById(R.id.hammer);
         pray.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                increment();
+                incrementPrayers();
+            }
+        });
+        hammer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                incrementHits();
             }
         });
 
         Toast.makeText(this,"Connecting to world...",Toast.LENGTH_SHORT).show();
-        fetchFromDB(0);
+        fetchFromDB();
         setCounterValue(getLocal());
         initAds();
     }
@@ -133,7 +156,7 @@ public class MainActivity extends AppCompatActivity {
         mAdView.loadAd(adRequest);
     }
 
-    private void fetchFromDB(final int newCount) {
+    private void fetchFromDB() {
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
@@ -144,6 +167,7 @@ public class MainActivity extends AppCompatActivity {
                     for (QueryDocumentSnapshot document : task.getResult()) {
                         Log.d(TAG, document.getId() + " => " + document.getData());
                         counter.setText(""+document.get("count"));
+                        hits.setText(""+document.get("hits"));
                     }
                 } else {
                     Log.w(TAG, "Error getting documents.", task.getException());
@@ -160,13 +184,22 @@ public class MainActivity extends AppCompatActivity {
         counter.setText(""+counterValue);
     }
 
-    public void increment(){
+    public void incrementPrayers(){
        String text= counter.getText().toString();
        int counterValue=Integer.parseInt(text);
        counterValue++;
-       incrementMyLocal();
+       incrementMyLocalPrayers();
        setCounterValue(counterValue);
     }
+
+    public void incrementHits(){
+        String text= hits.getText().toString();
+        int counterValue=Integer.parseInt(text);
+        counterValue++;
+        incrementMyLocalHits();
+        setCounterValue(counterValue);
+    }
+
 
 
 
